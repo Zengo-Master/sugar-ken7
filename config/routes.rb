cc6ecee6c5cc8bc3b7996190dd6af308e7b320ba
Zengo-Master <8435740@gmail.com>
2020-12-17 18:05:40 +0900

    results

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'users#index'
+  root to: 'results#index'
   resources :kifus, only: [:new, :create, :edit, :update, :show, :destroy]
   get 'results/:id', to: 'results#win_or_lose'
 end
