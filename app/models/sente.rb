d822c6ccd3959fa17dffc53163a8a8830ad416b0
Zengo-Master <8435740@gmail.com>
2020-12-18 21:48:15 +0900

    先後にメンバー追加

--- app/models/sente.rb
+++ app/models/sente.rb
@@ -7,5 +7,7 @@ class Sente < ActiveHash::Base
     { id: 5,  name: 'あつと' },
     { id: 6,  name: 'しゅん' },
     { id: 7,  name: 'しゅうまい' },
+    { id: 8,  name: 'TK' },
+    { id: 9,  name: 'れおす' }
   ]
 end
