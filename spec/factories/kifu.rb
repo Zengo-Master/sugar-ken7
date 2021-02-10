5d4382221d4f400875a5520be9376b97c27e9e58
Zengo-Master <8435740@gmail.com>
2021-02-10 17:16:21 +0900

    Update kifu.rb

--- spec/factories/kifu.rb
+++ spec/factories/kifu.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :kifu do
     Faker::Config.locale = :ja
-    date      { Faker::Date.between(from: '2020-01-01', to: '2021-12-31') }
+    date      { Faker::Date.between(from: '2021-01-01', to: '2021-12-31') }
     sente_id  { Faker::Number.between(from: 2, to: 7) }
     gote_id   { Faker::Number.between(from: 2, to: 7) }
     result_id { Faker::Number.between(from: 2, to: 5) }
